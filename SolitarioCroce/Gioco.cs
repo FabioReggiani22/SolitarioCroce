e75822432e4fdb5c0059c6658ac1f0a96865887f
Fabio Reggiani <FABIO.REGGIANI@local.ittcesena.it>
2024-04-09 12:54:43 +0200

    Added base file

---
+++ SolitarioCroce/Gioco.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SolitarioCroce
+{
+    public enum Mazzetto
+    {
+        BASE,
+        CROCE
+    }
+    public class Gioco
+    {
+        private Mazzo _mazzo;
+        private List<Carta> _pozzo;
+        private Carta[][] _basi;
+        private Carta[][] _croci;
+
+        public Carta[][] Basi
+        {
+            get => _basi;
+            set
+            {
+            }
+        }
+
+        public Carta[][] Croci
+        {
+            get => _croci;
+            set
+            {
+            }
+        }
+
+        public Mazzo Mazzo
+        {
+            get => _mazzo;
+            set
+            {
+            }
+        }
+
+        public List<Carta> Pozzo
+        {
+            get => _pozzo;
+            set
+            {
+            }
+        }
+
+        private void VerificaOrdineBase(Carta carta, int numeroBase)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void VerificaOrdineCroce(Carta carta, int numeroCroce)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SpostaCarte(Carta carta, Mazzetto mazzetto, int numeroMaz)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Vittoria()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+
+}
---
+++ SolitarioCroce/Mazzo.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolitarioCroce
+{
+    public class Mazzo
+    {
+        private Carta[] _carte;
+
+        public Mazzo()
+        {
+            _carte = new Carta[40];
+            InizializzaMazzo();
+           
+        }
+
+        private void InizializzaMazzo()
+        {
+            int i = 0;
+            for (int seme = 1; seme <= 4; seme++)
+            {
+                for (int valore = 1; valore <= 10; valore++)
+                {
+                    _carte[i] = new Carta(valore, (Seme)seme);
+                    i++;
+                }
+            }
+        }
+        public void MescolaMazzo()
+        {
+            Random random = new Random();
+            bool[,]presenti = new bool[4,10];
+            int i = 0;
+            while(i!=39)
+            {
+                Carta carta = new Carta(random.Next(1, 11), (Seme)random.Next(1, 5));
+                if (presenti[(int)carta.SemeCarta - 1, (int)carta.ValoreCarta - 1]) continue;
+                presenti[(int)carta.SemeCarta - 1, (int)carta.ValoreCarta - 1] = true;
+                i++;
+                _carte[i] = carta;
+            }
+        }
+
+
+        public Carta EstraiCarta
+        {
+            get
+            {
+                Random random = new Random();
+                Carta carta=_carte[random.Next(0,40)];
+                return carta;
+            }
+        }
+
+        public override string ToString()
+        {
+            string res = "";
+            for(int i=0; i< _carte.Length; i++) 
+            {
+                res += _carte[i].ToString();
+                res += "\n";
+            }
+            return res;
+        }
+    }
+}
