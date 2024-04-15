cdce2cf1ca9d369d7eed176614f33dac9d735194
alan.soldati <alan.soldati@PC0103>
2024-04-15 12:56:19 +0200

    Seconda modifica WPF (Nel precedente era presente un errore nel codice della wpf, infatti non era possibile avviare il programma)

--- Solitario/SolitarioWPF/Finestra Gioco.xaml.cs
+++ Solitario/SolitarioWPF/Finestra Gioco.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,18 +11,43 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SolitarioWPF
+namespace Solitario_A_Croce_WPF
 {
     /// <summary>
     /// Logica di interazione per Finestra_Gioco.xaml
     /// </summary>
     public partial class Finestra_Gioco : Window
     {
+        List<Button> _posizioniPresenti;
         public Finestra_Gioco()
         {
             InitializeComponent();
+            _posizioniPresenti = new List<Button>();
+            
+            RiempiPosizioni();
+            
+            
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+           
+        }
+
+        private void RiempiPosizioni()
+        {
+            ComboBox_SceltePosizioni.Items.Add("Mazzetto1");
+            ComboBox_SceltePosizioni.Items.Add("Mazzetto2");
+            ComboBox_SceltePosizioni.Items.Add("Mazzetto3");
+            ComboBox_SceltePosizioni.Items.Add("Mazzetto4");
+            ComboBox_SceltePosizioni.Items.Add("Mazzetto5");
+            ComboBox_SceltePosizioni.Items.Add("Base1");
+            ComboBox_SceltePosizioni.Items.Add("Base2");
+            ComboBox_SceltePosizioni.Items.Add("Base3");
+            ComboBox_SceltePosizioni.Items.Add("Base4");
         }
     }
 }
--- Solitario/SolitarioWPF/MainWindow.xaml.cs
+++ Solitario/SolitarioWPF/MainWindow.xaml.cs
@@ -12,8 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.WebRequestMethods;
 
-namespace SolitarioWPF
+namespace Solitario_A_Croce_WPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,6 +24,19 @@ namespace SolitarioWPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            
+            MessageBox.Show("", "Come giocare al solitario a croce.",MessageBoxButton.OK,MessageBoxImage.Information);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Finestra_Gioco finestra_Gioco= new Finestra_Gioco();
+            finestra_Gioco.Show();
         }
     }
 }
