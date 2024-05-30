69df1a9065576167a786139535934c5d89fc8a5a
Francesco Righi <FRANCESCO.RIGHI@local.ittcesena.it>
2024-05-30 10:45:07 +0200

    aggiunte ai test del gioco

--- Solitario/TestSolitario/UnitTestGioco.cs
+++ Solitario/TestSolitario/UnitTestGioco.cs
@@ -39,6 +39,36 @@ namespace TestSolitario
         }
 
         [TestMethod]
+        public void Gioco_SpostaCarte_WithInvalidMovement()
+        {
+            string id = "B1";
+            Mazzo mazzo = new Mazzo();
+            Gioco gioco = new Gioco(mazzo);
+            Assert.ThrowsException<ArgumentException>(() => gioco.SpostaCarte(gioco.Croci[1].VisualizzaPrimaCarta, id));
+        }
+
+        [TestMethod]
+        public void Gioco_SpostaCarte_WithInvalidCarta()
+        {
+
+            Mazzo mazzo = new Mazzo();
+            Gioco gioco = new Gioco(mazzo);
+            Carta carta = null;
+            Assert.ThrowsException<ArgumentException>(() => gioco.SpostaCarte(carta, "aa"));
+        }
+
+        [TestMethod]
+        public void Gioco_SpostaCarte_VerificaSeLaCartaVieneSpostata()
+        {
+
+            Mazzo mazzo = new Mazzo();
+            Gioco gioco = new Gioco(mazzo);
+            Carta carta = gioco.Croci[0].VisualizzaPrimaCarta;
+            gioco.SpostaCarte(gioco.Croci[0].VisualizzaPrimaCarta, "B1");
+            Assert.AreEqual(gioco.Basi[0].VisualizzaPrimaCarta,carta) ;
+        }
+
+        [TestMethod]
         public void Gioco_PescaCarta()
         {
             Mazzo mazzo = new Mazzo();
@@ -47,5 +77,18 @@ namespace TestSolitario
             gioco.PescaCarta();
             Assert.AreEqual(gioco.Pozzo[0], carta);
         }
+
+        [TestMethod]
+        public void Gioco_PescaCarta_WithMazzoVuoto()
+        {
+            Mazzo mazzo = new Mazzo();
+            Gioco gioco = new Gioco(mazzo);
+            for(int i = 0; i< 35; i++)
+            {
+                gioco.PescaCarta();
+            }
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => gioco.PescaCarta());
+        }
+
     }
 }
